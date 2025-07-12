2b1f6a238d76eaa32965264277a9bbf1200c4288
sorjam <soroush.rubik@gmail.com>
2025-07-12 19:05:36 +0330

    Add files via upload

---
+++ style/main.js
@@ -0,0 +1,15 @@
+/*document.getElementById("AirPods").addEventListener("mouseenter", mouseenter);
+function mouseenter(){
+    let Airpods =document.getElementById("AirPods")
+    Airpods.src = "../img/img/Apple_AirPods_Max_Silver_2.jpg"
+}*/
+document.getElementById("AirPods").addEventListener("mouseenter", mouseenter);
+
+
+let airpods = document.getElementById("AirPods");
+airpods.src= "style/airpds.jpg"
+
+
+
+
+
